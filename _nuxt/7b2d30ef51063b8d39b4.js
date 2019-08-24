cf6d47a3d3984fd31f2382aadb9ddea5d8965d9b
Erdem Bircan <erdem.erdembircan@gmail.com>
2019-08-24 17:48:57 +0300

    initial commit

---
+++ _nuxt/7b2d30ef51063b8d39b4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{145:function(e,n,t){"use strict";t.r(n);var l=t(22),component=Object(l.a)({},function(){var e=this.$createElement;return(this._self._c||e)("i",[this._v("there was a hole here, it's gone now...")])},[],!1,null,null,null);n.default=component.exports}}]);
